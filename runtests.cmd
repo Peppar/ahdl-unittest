3c550d1cc1d130b0e25a99ff137cee8de3482971
Oskar Holstensson <oskar@holstensson.se>
2020-01-07 21:51:56 +0100

    Fix runtests.cmd to not show error message if test.log does not exist

--- runtests.cmd
+++ runtests.cmd
@@ -4,7 +4,7 @@ set AHDL_BIN=C:\lscc\diamond\3.10_x64\active-hdl\BIN
 set PATH=%AHDL_BIN%;%PATH%
 
 rem The test log is recreated by test.do
-del test.log
+del test.log 2>NUL
 
 rem -do is like -execute but it does not quit the GUI when the script stops
 avhdl -quiet -nosplash -execute "runtests.do"
